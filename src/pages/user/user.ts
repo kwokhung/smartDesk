ec695f2eeab60911dfdef72aacad6c3686ff096b
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-07-05 15:43:14 +0800

    init

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { UserHelper } from '../../providers/user-helper/user-helper';
 
 @IonicPage({
@@ -13,7 +13,7 @@ export class UserPage {
 
   users: Array<any> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public userHelper: UserHelper) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public userHelper: UserHelper) {
   }
 
   ionViewDidLoad() {
@@ -36,6 +36,28 @@ export class UserPage {
 
   addUser() {
     this.navCtrl.push('AddUserPage');
+  }
+
+  deleteUser(user) {
+    this.userHelper.deleteUser(user.samAccountName).then((data) => {
+      this.toastCtrl.create({
+        message: 'User is deleted',
+        duration: 1000,
+        position: 'middle'
+      }).present();
+
+      this.navCtrl.push('UserPage');
+    });
+  }
+
+  resetPassword(user) {
+    this.userHelper.resetPassword(user.samAccountName).then((data) => {
+      this.toastCtrl.create({
+        message: 'Password is reset',
+        duration: 1000,
+        position: 'middle'
+      }).present();
+    });
   }
 
 }
