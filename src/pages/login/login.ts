ebe5ecc21bbb6e5ffc44cb538845f6322b3f25ea
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-07-12 14:04:24 +0800

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { Logger } from '../../providers/logger/logger';
+import { AuthService } from '../../providers/auth-service/auth-service';
 
 @IonicPage({
   name: 'LoginPage'
@@ -11,7 +12,10 @@ import { Logger } from '../../providers/logger/logger';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public logger: Logger) {
+  loading: Loading;
+  registerCredentials = { email: '', password: '' };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public loadingCtrl: LoadingController, public auth: AuthService, public logger: Logger) {
     logger.addLog('LoginPage');
   }
 
@@ -19,4 +23,41 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  public createAccount() {
+    this.navCtrl.push('RegisterPage');
+  }
+
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {
+        this.navCtrl.setRoot('HomePage');
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+  
 }
